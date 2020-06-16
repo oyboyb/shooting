87730c198326c53a6074e299e3266d39c9f2a1d5
oyboyb <761231629@qq.com>
2020-06-16 18:20:40 +0800

    保存

--- src/components/GameContainer.vue
+++ src/components/GameContainer.vue
@@ -1,6 +1,9 @@
 <template>
  <div class="game-container">
    <enemy v-for=" (key,index) in 2" :key="index" :x="Math.round((Math.random()*380)+1)" :y="0"></enemy>
+   <div class="player">
+     <span class="bullet"></span>
+   </div>
  </div>
 </template>
 <script>
@@ -25,24 +28,48 @@ export default {
   mounted(){
   },
   methods:{
+    shoot(){
+    },
     onkeydown(e){
-      debugger;
       console.log(e)
       for(let i=0,len=this.$children.length;i<len;i++){
           let item=this.$children[i]
           if(e.key===item.str)item.killed=!item.killed,item.timer=null
+          this.shoot()
       }
     }
   }
 }
 </script>
-
 <style scoped>
 .game-container{
   position:relative;
   width:400px;
   height:600px;
   margin:10px auto;
-  background: green;
+  background: #ddd;
+}
+.player{
+  position: absolute;
+  transform:translate(-50%,-60px);
+  top:100%;
+  left:50%;
+  width:60px;
+  height:70px;
+  background-image:url('../assets/player.gif');
+  background-size:contain;
+  background-repeat: no-repeat;
+}
+.bullet{
+  position: absolute;
+  top:-30px;
+  left:50%;
+  transform: translateX(-50%);
+  width:15px;
+  height:25px;
+  background-image:url('../assets/bullet.gif');
+  background-size:contain;
+  background-repeat: no-repeat;
+  background-position:center;
 }
 </style>
